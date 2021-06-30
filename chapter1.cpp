5ce019601d455bd24d9094e0cc95aa15d809cbcc
ClaireGuerin <guerin.claire01@gmail.com>
2021-06-30 09:56:14 +0200

    Done chapter 1 with basic calculations and prime number machine, onto chapter 2

--- chapter1.cpp
+++ chapter1.cpp
@@ -16,9 +16,14 @@ Interpret and explain the result. */
 // Prime number machine (?).
 /* In this exercise, you will create an incredibly useful prime number machine that will allow you generate 40 different prime numbers. 
 Write your program in such a way that it first asks the user to enter a natural number less than forty. 
-Use this number n to generate a prime number P (n) using the formula P (n) = n 2 + n + 41
-and write the resulting number to the screen. 
-Hint: use the multiplication operator * to compute the square of the number n (i.e., n 2 = n * n ). */
+Use this number n to generate a prime number P(n) using the formula P(n) = n^2 + n + 41 and write the resulting number to the screen. 
+Hint: use the multiplication operator * to compute the square of the number n (i.e., n^2 = n * n ). */
+
+int primer(int num) {
+    // For numbers less than 40:
+    int p = num * num + num + 41;
+    return p;
+}
 
 int main() {
     int i, j;
@@ -30,8 +35,22 @@ int main() {
     std::cout << "Addition: " << i + j << "\n";
     std::cout << "Subtraction: " << i - j << "\n";
     std::cout << "Mutiplication: " << i * j << "\n";
+    // DIVIDING INTEGERS
+    /* When integers are divided, the result of the / operator is the algebraic quotient with any fractional part discarded. 
+    If the quotient a/b is representable, the expression (a/b)*b + a%b shall equal a. 
+    This is often called "truncation toward zero".
+    The usual arithmetic conversions are performed on the operands.
+    The result of the / operator is the quotient from the division of the first operand by the second; 
+    the result of the % operator is the remainder. 
+    In both operations, if the value of the second operand is zero, the behavior is undefined.*/
     std::cout << "Division: " << i / j << "\n";
     std::cout << "Modulo: " << i % j << "\n";
+
+    // PRIME NUMBER MACHINE
+    int n;
+    std::cout << "Give me a number < 40: \n";
+    std::cin >> n;
+    std::cout << "Here's a prime number: " << primer(n) << "\n";
 
     return 0;
 }
