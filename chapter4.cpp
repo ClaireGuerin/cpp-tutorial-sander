0ed6082c3b32b8ee82b8d8445e9c741b01cc00f8
ClaireGuerin <guerin.claire01@gmail.com>
2021-07-01 09:58:39 +0200

    finished chapter 4 with switch statements, onto chapter 5

--- chapter4.cpp
+++ chapter4.cpp
@@ -39,6 +39,47 @@ no real-valued solution if b 2 < 4ac.
 Also, the equation has a single solution if a = 0 and b != 0. 
 Extend your solution of exercise 3.5 and ensure that your program deals correctly with all of the above cases. */
 
+// The conditional operator.
+/* Use the conditional operator to replace the if - else statement in the following program fragment: 
+std::cout << "Please enter a number: ";
+double dVal;
+std::cin >> dVal;
+double dClip = dVal;
+if(dVal < 0.0) dClip = 0.0; */
+
+// The switch-statement.
+/* Replace the if - else ladder in the following program fragment by a switch - statement 
+
+std::cout << "Who is most resistant to alcohol?" << std::endl
+<< "(1) Master Yoda" << std::endl
+<< "(2) E.T." << std::endl
+<< "(3) Nefertiti" << std::endl
+<< "(4) Arnold Schwarzenegger" << std::endl
+<< "(5) Jamie Oliver" << std::endl
+<< "Your answer: ";
+int iAnswer;
+std::cin >> iAnswer;
+
+if (iAnswer == 1 || iAnswer == 2) 
+    std::cout << "wrong! (extraterrestrials don't drink alcohol)" << std::endl;
+else if (iAnswer == 3)
+    std::cout << "wrong! (Nefertiti only drinks the milk of she-donkeys)" << std::endl;
+else if (iAnswer == 4)
+    std::cout << "correct!" << std::endl;
+else if (iAnswer == 5)
+    std::cout << "wrong! (Jamie has a sip of wine now and then, but usually spits it out after tasting)" << << std::endl;
+else
+    std::cout << "invalid answer" << std::endl;*/
+
+// The bitwise logical operators.
+/* Table 3.1 indicates that there are three (binary) bitwise logical operators, & and | and ^ , 
+and a (unary) bitwise complement operator ~ . 
+These need to be carefully distinguished from the standard logical operators && , || and ! . 
+The bitwise logical operators take integer operands and their result is an integer as well. Analyse the behaviour of the four operators by examining
+the output produced by the following code. Try to make sense of the results by first writing the numbers
+before and after the bitwise operation in binary notation. Next, formulate and test your hypothesis on 
+the effect of the bitwise logical operators */
+
 string identifyCritter(const bool flies, const int nLegs) {
     string critter;
     if (flies || nLegs == 6) {
@@ -126,6 +167,54 @@ int main() {
     cout << "You're " << ratePessimism(a1, a2, a3) << "\n";
 
     findRoots(-10, 0, 50);
+
+    std::cout << "Please enter a number: ";
+    double dVal;
+    std::cin >> dVal;
+    const double dClip = (dVal < 0.0) ? 0.0: dVal;
+    cout << dClip << "\n";
+
+    std::cout << "Who is most resistant to alcohol?" << std::endl
+    << "(1) Master Yoda" << std::endl
+    << "(2) E.T." << std::endl
+    << "(3) Nefertiti" << std::endl
+    << "(4) Arnold Schwarzenegger" << std::endl
+    << "(5) Jamie Oliver" << std::endl
+    << "Your answer: ";
+    int iAnswer;
+    std::cin >> iAnswer;
+
+    switch (iAnswer) {
+        case 1: case 2:
+            std::cout << "wrong! (extraterrestrials don't drink alcohol)" << std::endl;
+            break;
+        case 3:
+            std::cout << "wrong! (Nefertiti only drinks the milk of she-donkeys)" << std::endl;
+            break;
+        case 4:
+            std::cout << "correct!" << std::endl;
+            break;
+        case 5:
+            std::cout << "wrong! (Jamie has a sip of wine now and then, but usually spits it out after tasting)" << std::endl;
+            break;
+        default:
+            std::cout << "invalid answer" << std::endl;
+    }   
+
+    const int i = 12, j = 10;
+    std::cout << "i & j = " << (i & j) << endl; // i & j has value 8 (binary 0000 1000)
+    std::cout << "i | j = " << (i | j) << endl; // i | j has value 14 (binary 0000 1110)
+    std::cout << "i ^ j = " << (i ^ j) << endl; // i ^ j has value 6 (binary 0000 0110)
+    std::cout << "~i = " << (~i) << endl; // ~i has value -13 (binary 1111 0011)
+
+    /* the bitwise operators & , | and ^ apply, respectively, the logical operations AND, OR and XOR on a bit-by-bit basis on a pair of two integers. 
+    For example, the bitwise AND operator ( & ) compares each bit of the first operand to the corresponding bit of the second operand.
+    If both bits are 1, the corresponding result bit is set to 1. Otherwise, the corresponding result bit is set to 0. 
+    The unary bitwise complement operator ~ is the bitwise equivalent of the standard logical NOT (operator ! ): 
+    for each bit in its operand the corresponding bit in the result is set to the complementary value. 
+    The resulting bit pattern is interpreted on most platforms according to the so-called two’s complement representation of signed numbers, 
+    which interprets the first bit of the pattern as a sign bit (0 for positive number, 1 for negative numbers). 
+    According to this representation, ~i then evaluates to the number -(i + 1) using ordinary arithmetic.*/
 
     return 0;
 }
